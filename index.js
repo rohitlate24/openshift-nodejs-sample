f9c549406a9f0d0b17efff6a2f72570f8175448f
rohitlate24 <123583749+rohitlate24@users.noreply.github.com>
2023-01-28 22:07:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from OpenShift cli-build!');
+  res.end('Hello World from OpenShift cli-build! New upaded page');
 });
 server.listen(8080, function() {
 console.log('Server running on port 8080');
